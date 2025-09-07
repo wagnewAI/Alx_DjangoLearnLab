9cf43722f86c367f6257e83bbb4943178017c921
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-09-07 21:32:14 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView
-from .models import  Author
-from .models import  Book
-from .models import  Library
-from .models import  Librarian
-from .models import  UserProfile
+from .models import Author
+from .models import Book
+from .models import Library
+from .models import Librarian
+from .models import UserProfile
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth import logout
