4fae52fa2dcadc706da447183ab1c971fac6d509
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-09-07 20:52:41 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
     # Secured book operations
-    path("book/add/", views.add_book, name="add_book"),
+    path("add_book/", views.add_book, name="add_book"),
     path("book/edit/<int:pk>/", views.edit_book, name="edit_book"),
-    path("book/delete/<int:pk>/", views.delete_book, name="delete_book"),
+    path("edit_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
