0cf5471479dfcb437cb2d57209971f858e42d076
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-09-07 21:29:59 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
@@ -6,8 +7,8 @@ urlpatterns = [
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # CBV
     # Authentication URLs
     path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
       # Role-based URLs
     path("admin-view/", views.admin_view, name="admin_view"),
     path("librarian-view/", views.librarian_view, name="librarian_view"),
