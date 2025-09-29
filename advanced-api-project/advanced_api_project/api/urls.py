4ca0000cc90ada3c7cbb1cf2ff80eedab8a48ae1
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-09-29 13:58:00 +0300

    Update urls.py

--- advanced-api-project/advanced_api_project/api/urls.py
+++ advanced-api-project/advanced_api_project/api/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path("api/books/", BookListView.as_view(), name="book-list"),
-    path("api/books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
-    path("api/books/create/", BookCreateView.as_view(), name="book-create"),
-    path("api/books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),
-    path("api/books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
+    path("/books/", BookListView.as_view(), name="book-list"),
+    path("/books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
+    path("/books/create/", BookCreateView.as_view(), name="book-create"),
+    path("/books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),
+    path("/books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
 ]
