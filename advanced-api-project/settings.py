70a091457f3ef6d54f1a8767c59aed092bdfbd66
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-10-01 19:55:54 +0300

    Update settings.py

--- advanced-api-project/settings.py
+++ advanced-api-project/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'advanced_api_project.urls'
+ROOT_URLCONF = 'advanced-api-project.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'advanced_api_project.wsgi.application'
+WSGI_APPLICATION = 'advanced-api-project.wsgi.application'
 
 
 # Database
