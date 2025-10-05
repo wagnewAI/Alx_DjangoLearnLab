a86d4a000b3b423a7d9d47d9db7b14bf9ea75194
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-10-05 23:05:03 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
 
     # Blog posts CRUD
     path('posts/', views.PostListView.as_view(), name='posts'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
       # Comment URLs
     path('post/<int:pk>/comments/new/', views.add_comment, name='comment-add'),
