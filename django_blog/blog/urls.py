aca15a713dbb6175fbd4cbf5de52c3fa875fe212
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-10-05 23:43:28 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     # search urls
     path('search/', views.search_posts, name='search'),
     path('tags/<str:tag_name>/', views.posts_by_tag, name='posts-by-tag'),
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts_by_tag'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
 ]
